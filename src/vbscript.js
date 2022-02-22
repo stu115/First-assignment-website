b62f3cef13240d9c68b246080fec019366002938
Stefan Ahmed <32064735+stu115@users.noreply.github.com>
2022-02-22 00:03:32 +0000

    Add files via upload

---
+++ src/vbscript.js
@@ -0,0 +1,78 @@
+<script>
+
+function fillForm(){
+
+var x = document.form["orderform"]["fname"].value;
+
+if (x == ""){
+   alert("Please enter your first name");
+    return false;	
+})
+
+var y = document.form["orderform"]["lname"].value;
+
+if (y == ""){
+	alert("Please enter your last name too");
+	 return false;
+})
+
+var z = document.form["orderform"]["telnumber"].value;
+
+if (z == ""){
+    alert("Please enter your telephone number");
+     return false;
+})	 
+
+var re = /+w[]
+
+  if (!re. ["orderform"]["emailaddress"].value;){
+      window.alert("Please enter a valid email address");
+        return false;
+})
+
+var b = document.form["orderform"]["facilities"].value;
+
+  if (a == ""){
+      alert("Choose a specific");
+        return false;
+})
+
+var i = document.form["orderform"]["facilities"].value;
+
+  if (a == ""){
+      alert("Choose a specific");
+        return false;
+})
+
+var s = document.form["orderform"]["facilities"].value;
+
+  if (a == ""){
+      alert("Choose a specific");
+        return false;
+})
+
+var pool = document.form["orderform"]["facilities"].value;
+  if (a == ""){
+      alert("Choose a specific");
+        return false;
+})
+
+var g = document.form["orderform"]["facilities"].value;
+
+  if (a == ""){
+      alert("Choose a specific");
+        return false;
+})
+
+var arom = document.form["orderform"]["facilities"].value;
+
+  if (a == ""){
+      alert("Choose a specific");
+        return false;
+})
+
+
+
+		
+}
+</script>
